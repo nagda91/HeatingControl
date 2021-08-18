91861a98670dd05315272ef5304a34c0be0708f2
Dávid Nagy <35817702+nagda91@users.noreply.github.com>
2021-08-18 21:57:54 +0200

    Update main.cpp

--- HeatingControl/src/main.cpp
+++ HeatingControl/src/main.cpp
@@ -9,21 +9,15 @@
 #include <thread>
 #include <mosquittopp.h>
 #include <mosquitto.h>
-
 #include "Core.h"
 
 #define CLIENT_ID "HeatingControl"
 #define MQTT_PORT 1883;
 #define MQTT_TOPIC "topic"
-#define MQTT_USER "thenewone"
-#define MQTT_PWD "1991"
-//#define SETTINGS_FILE "/home/pi/projects/HeatingControl/bin/ARM/Release/settings.txt"
-
-//int mainpipe, boiler1, boiler2, heater, house, chimney, solar, solarboiler;
+#define MQTT_USER "USR"
+#define MQTT_PWD "PSW"
 
 using namespace std;
-
-//web		"/home/pi/Desktop/cb/log/strekszel.txt"
 
 int main() {
 
@@ -40,56 +34,23 @@ int main() {
 	/////wiringPi/////
 
 	////MQTT/////
-
-
 	class Core* iot_client;
 	//Connection data
 	char client_id[] = CLIENT_ID;
-	/*string mqttSet, mqttuser, mqttpwd;
-	ifstream settings;
-	settings.open(SETTINGS_FILE);
-	if (settings.fail()) cout << "Could not open the settings file! :(\n";
-	else {
-		string mqttSet;
-		do {
-
-			getline(settings, mqttSet);
-			if(mqttSet.find("#") != -1) getline(settings, mqttSet);
-			if (mqttSet.find("-mqttUSER") != -1) {
-				mqttuser = mqttSet.substr(mqttSet.find_first_of(' ') + 1, mqttSet.length()- mqttSet.find_first_of(' '));
-				cout << "MQTT username: " << mqttuser << "MMM" << endl;
-			}
-			else if (mqttSet.find("-mqttPWD") != -1) {
-				mqttpwd = mqttSet.substr(mqttSet.find_first_of(' ') + 1, mqttSet.length() - mqttSet.find_first_of(' '));
-				cout << "MQTT password: " << mqttpwd << "MMM" << endl;
-			}
-
-		} while (!settings.eof() || (!mqttuser.empty() && !mqttpwd.empty()));
-
-	}*/
-
 	const char* USER = MQTT_USER;
 	const char* PASSW = MQTT_PWD;
-	//int sadf = 13245;
-	//int* clint_id = &sadf;
 	char host[] = "localhost";
 	int port = MQTT_PORT;
 	string topicString = MQTT_TOPIC;
-
 	mosqpp::lib_init();
-
 	iot_client = new Core(client_id, host, port, USER, PASSW);
-	/*cout << "usr pw: " << iot_client->username_pw_set(USER, PASSW);
-	cout << "sub: " << iot_client->subscribe(clint_id, MQTT_TOPIC, 2);
-	cout << "loop: " << iot_client->loop_start();*/
-	//iot_client->username_pw_set(USER, PASSW);
-	//iot_client->subscribe(clint_id, MQTT_TOPIC, 2);
-	//iot_client->subscribe(clint_id, "extsensors", 2);
 	iot_client->loop_start();
 	////MQTT/////
 
+	//Starting the main thread
 	std::thread mainthread = iot_client->basicFuncthread();
 
+	//Starting the user interface
 	string comm;
 	do
 	{
